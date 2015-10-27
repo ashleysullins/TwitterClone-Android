606a9c0a4bf9a3420c2cbe3279741855176fc66a
nifferdil <nifferdil@gmail.com>
2015-10-27 10:03:35 -0700

    "add user login"

--- app/src/main/java/com/example/guest/twitter_clone/models/User.java
+++ app/src/main/java/com/example/guest/twitter_clone/models/User.java
@@ -3,6 +3,7 @@ package com.example.guest.twitter_clone.models;
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
 
 /**
  * Created by Guest on 10/27/15.
@@ -27,4 +28,10 @@ public class User extends Model {
         return mName;
     }
 
+    public static User find(String username) {
+        return new Select()
+                .from(User.class)
+                .where("Name = ?", username)
+                .executeSingle();
+    }
 }
---
+++ app/src/main/java/com/example/guest/twitter_clone/ui/RegisterActivity.java
@@ -0,0 +1,65 @@
+package com.example.guest.twitter_clone.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.guest.twitter_clone.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mNameEdit;
+    private Button mLogInButton;
+    private SharedPreferences mPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mNameEdit = (EditText) findViewById(R.id.nameText);
+        mLogInButton = (Button) findViewById(R.id.registerButton);
+        mPreferences = getApplicationContext().getSharedPreferences("twitter", Context.MODE_PRIVATE);
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameEdit.getText().toString();
+                SharedPreferences.Editor editor = mPreferences.edit();
+                editor.putString("username", name);
+                editor.commit();
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
