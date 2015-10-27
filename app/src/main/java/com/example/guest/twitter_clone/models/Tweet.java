03c961198b5895d9b54f562be2958d1037e7f8f0
nifferdil <nifferdil@gmail.com>
2015-10-27 09:34:38 -0700

    "add Tweet and User classes"

---
+++ app/src/main/java/com/example/guest/twitter_clone/models/Tweet.java
@@ -0,0 +1,64 @@
+package com.example.guest.twitter_clone.models;
+
+import android.content.Context;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.example.guest.twitter_clone.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Guest on 10/27/15.
+ */
+@Table(name = "tweet", id = "_id")
+public class Tweet extends Model{
+
+    @Column(name = "Content")
+    private String mContent;
+
+    @Column(name = "CreateAt")
+    private long mCreatedAt;
+
+    @Column(name = "User")
+    private User mUser;
+
+    public Tweet() {
+        super();
+    }
+
+    public Tweet(String content, User user) {
+        mContent = content;
+        mUser = user;
+        mCreatedAt = new Date().getTime();
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+
+    public long getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    public User getUser() {
+        return mUser;
+    }
+
+    public void setUser(User user) {
+        mUser = user;
+    }
+
+    public String getFormattedTime(Context context) {
+        SimpleDateFormat formatter = new SimpleDateFormat(context.getString(R.string.formatted_time));
+        formatter.setTimeZone(TimeZone.getTimeZone(context.getString(R.string.timezone)));
+        return formatter.format(mCreatedAt);
+    }
+}
